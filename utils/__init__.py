b98751c71bd647ec3a3d601239e48f8926b210ab
Admin9705 <9705@duck.com>
2025-04-05 11:06:27 -0400

    update

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,23 +1,3 @@
-# missing/__init__.py
-"""
-Missing content search functionality for Huntarr-Lidarr
-"""
-
-from missing.artist import process_artists_missing
-from missing.album import process_albums_missing
-
-__all__ = ['process_artists_missing', 'process_albums_missing']
-
-# upgrade/__init__.py
-"""
-Upgrade functionality for Huntarr-Lidarr
-"""
-
-from upgrade.track import process_cutoff_upgrades
-
-__all__ = ['process_cutoff_upgrades']
-
-# utils/__init__.py
 """
 Utility functions for Huntarr-Lidarr
 """
